008f50630ffe76b6748ef33ffd051c0379d87ab3
unknown <i.sude.adiguzel@gmail.com>
2021-01-20 11:52:37 +0300

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            int Hour = DateTime.Now.Hour;
+           
+            ViewBag.Greeting= Hour > 12 ? "İyi Günler." : "Günaydın";
+            ViewBag.UserName = "Sude Adıgüzel";
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View("MyView");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
